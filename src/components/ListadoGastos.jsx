edc35dfa6eb8885e8fddf2b6d7386d9c5b91a704
jakdax-22 <enriqueiranzomartinez4@gmail.com>
2023-08-05 01:39:09 +0200

    V1

---
+++ src/components/ListadoGastos.jsx
@@ -0,0 +1,40 @@
+import Gasto from "./Gasto"
+
+const ListadoGastos = ({gastos,setGastoEditar,eliminarGasto,filtro,gastosFiltrados}) => {
+  return (
+    <div>
+        <div className="Listado-gastos contenedor">
+            {
+              filtro ? (
+                <>
+                  <h2>{gastosFiltrados.length ? 'Gastos' : "No hay gastos aún"}</h2>
+                  {gastosFiltrados.map(gasto => (
+                    <Gasto 
+                        key={gasto.id}
+                        gasto={gasto}
+                        setGastoEditar={setGastoEditar}
+                        eliminarGasto={eliminarGasto}
+                    />
+              ))}
+                </>
+              ) : (
+                <>
+                  <h2>{gastosFiltrados.length ? 'Gastos' : "No hay gastos aún"}</h2>
+                  {gastos.map(gasto => (
+                    <Gasto 
+                        key={gasto.id}
+                        gasto={gasto}
+                        setGastoEditar={setGastoEditar}
+                        eliminarGasto={eliminarGasto}
+                    />
+              ))}
+                </>
+              )
+            }
+            
+        </div>
+    </div>
+  )
+}
+
+export default ListadoGastos
